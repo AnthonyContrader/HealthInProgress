11a5141a3d26dea1429caeb6c352f8cd0cdbb6f6
Abdel123456 <abdelbasset.marghli@live.fr>
2019-06-08 23:47:47 +0200

    aggiungi

--- src/main/java/it/contrader/view/parameters/ParametersUpdateView.java
+++ src/main/java/it/contrader/view/parameters/ParametersUpdateView.java
@@ -1,15 +1,13 @@
 package it.contrader.view.parameters;
 
 import java.util.List;
-
-
 import java.util.Scanner;
 
 import it.contrader.controller.Request;
-import it.contrader.controller.ParametersController;
-import it.contrader.dto.ParametersDTO;
+import it.contrader.controller.*;
+import it.contrader.dto.*;
 import it.contrader.main.MainDispatcher;
-import it.contrader.model.Parameters;
+import it.contrader.model.*;
 import it.contrader.view.View;
 
 public class ParametersUpdateView implements View {
@@ -27,59 +25,60 @@ public class ParametersUpdateView implements View {
 
 	@Override
 	public void showOptions() {
-		int parametersIdToUpdate;
-		String sesso;
-		int eta, polso, freqres, tempcorp;
-		double altezza, peso;
+		int parameterIdToUpdate,eta,polso,ferqResp,tempCorp;
+		double altezza,peso;
+		String sesso ;
 
 		ParametersDTO parametersDTO = new ParametersDTO();
 
-		System.out.println("Digita l'Id parametro da modificare:");
+		System.out.println("Digita l'Id dell parameter da modificare:");
 		try {
-			parametersIdToUpdate = Integer.parseInt(getInput());
-			if (parametersIdToUpdate != 0) {
-				parametersDTO.setIdParameter(parametersIdToUpdate);
-				
-				
-				System.out.println("Digita nuovo sesso:");
+			parameterIdToUpdate = Integer.parseInt(getInput());
+			if (parameterIdToUpdate != 0) {
+				parametersDTO.setIdParameter(parameterIdToUpdate);
+
+				System.out.println("Digita il sesso:");
 				sesso = getInput();
-
 				if (!sesso.equals(""))
-					parametersDTO.setSesso(sesso);
-				System.out.println("Digita la nuova altezza:");
-				altezza = Double.parseDouble(getInput());
+					parametersDTO.getSesso();
 				
-				if (altezza != 0)
-					
-				parametersDTO.setAltezza(altezza);
+				System.out.println("Digita la nuova altezza");
+				altezza = Double.parseDouble(getInput());
+				if (altezza !=0)
+					parametersDTO.setAltezza(altezza);
 
-				System.out.println("Digita nuovo peso:");
+			
+				System.out.println("Digita il nuovo peso");
 				peso = Double.parseDouble(getInput());
-				if (peso != 0)
-				parametersDTO.setPeso(peso);
-				System.out.println("Digita nuovo eta:");
+				if (peso !=0)
+					parametersDTO.setPeso(peso);
+				
+				System.out.println("Digita la nuova eta");
 				eta = Integer.parseInt(getInput());
-				if (eta != 0)
-				parametersDTO.setEta(eta);
-
-				System.out.println("Digita nuovo peso:");
-				peso = Integer.parseInt(getInput());
-				if (peso != 0)
-				parametersDTO.setPeso(peso);
-				System.out.println("Digita nuovo polso:");
+				if (eta !=0)
+					parametersDTO.setEta(eta);
+				
+				
+				System.out.println("Digita la nuova poslo");
 				polso = Integer.parseInt(getInput());
-				if (polso != 0)
-				parametersDTO.setPolso(polso);
-				System.out.println("Digita nuovo freqResp:");
-				freqres = Integer.parseInt(getInput());
-				if (freqres != 0)
-				parametersDTO.setFreqResp(freqres);
-				System.out.println("Digita nuovo tempCorp:");
-				tempcorp = Integer.parseInt(getInput());
-				if (tempcorp != 0)
-				parametersDTO.setTempCorp(tempcorp);
+				if (polso !=0)
+					parametersDTO.setEta(polso);
+				
+				
+				System.out.println("Digita la nuova ferResp");
+				ferqResp = Integer.parseInt(getInput());
+				if (ferqResp !=0)
+					parametersDTO.setFreqResp(ferqResp);
+				
+				System.out.println("Digita la tempCorp");
+				tempCorp= Integer.parseInt(getInput());
+				if (tempCorp !=0)
+					parametersDTO.setTempCorp(tempCorp);
+				
+
 
 				parametersController.updateParameters(parametersDTO);
+				
 
 			}
 		} catch (Exception e) {
@@ -90,9 +89,8 @@ public class ParametersUpdateView implements View {
 
 	@Override
 	public String getInput() {
-		Scanner in = new Scanner(System.in);
-		int num = in.nextInt();
-		return in.nextLine().trim();
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine().trim();
 	}
 
 	@Override
@@ -100,7 +98,7 @@ public class ParametersUpdateView implements View {
 		request = new Request();
 		request.put("mode", "menu");
 		request.put("choice", "");
-		MainDispatcher.getInstance().callAction("Parameters", "doControl", request);
+		MainDispatcher.getInstance().callAction("User", "doControl", request);
 	}
 
 }
